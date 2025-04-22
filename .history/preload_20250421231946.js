82f56333ef205a88a439460f13b132b938a44219
hsonthach <son.thach011011@gmail.com>
2025-04-22 14:05:20 -0500

    Remove historical files and examples from the project, including environment variables, HTML, JavaScript, CSS, and package configurations.

--- .history/preload_20250421231946.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("aiBrowser", {
-  executeCommand: (command) => ipcRenderer.invoke("execute-command", command),
-  onPlanUpdate: (callback) =>
-    ipcRenderer.on("plan-update", (event, plan) => callback(plan)),
-});
